042da16584e52ffaef657aa0492adc81bb996bf0
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2018-04-20 15:25:29 +0200

    change lockscreen picture for eDP1 screen

--- i3lock.sh
+++ i3lock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if (xrandr --listactivemonitors | grep eDP-1); then
-	i3lock -c 000000 -i ~/Pictures/wallpaper/5ccc5e0f258ff34f.png
+	#i3lock -c 000000 -i ~/Pictures/wallpaper/5ccc5e0f258ff34f.png
+	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_nb.png
 else
 	i3lock -i ~/Pictures/wallpaper/23520575262_34d522db9e_o.png
 fi
