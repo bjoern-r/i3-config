54700178385a765368e9464ed898631df260081b
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2018-04-26 14:43:38 +0200

    add suspend support to i3lock wrapper

--- i3lock.sh
+++ i3lock.sh
@@ -3,5 +3,8 @@ if (xrandr --listactivemonitors | grep eDP-1); then
 	#i3lock -c 000000 -i ~/Pictures/wallpaper/5ccc5e0f258ff34f.png
 	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_nb.png
 else
-	i3lock -i ~/Pictures/wallpaper/23520575262_34d522db9e_o.png
+	[ $(date +%H%M) -ge 1815 ] && DPMS=" -d"
+	i3lock -i ~/Pictures/wallpaper/23520575262_34d522db9e_o.png $DPMS
 fi
+
+[ x"$1" = "xsuspend" ] && systemctl suspend
