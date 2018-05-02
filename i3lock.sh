bc7585f2bd7d3c4581139c27fb59f0464a08a0b9
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2018-05-02 18:12:01 +0200

    dont use dmps if going to suspend && xautolock

--- i3lock.sh
+++ i3lock.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 if (xrandr --listactivemonitors | grep eDP-1); then
-	#i3lock -c 000000 -i ~/Pictures/wallpaper/5ccc5e0f258ff34f.png
 	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_nb.png
 else
-	[ $(date +%H%M) -ge 1815 ] && DPMS=" -d"
-	i3lock -i ~/Pictures/wallpaper/23520575262_34d522db9e_o.png $DPMS
+	[ $(date +%H%M) -ge 1815 ] && [ x"$1" != "xsuspend" ] && DPMS=" -d"
+	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_desktop.png $DPMS
 fi
 
 [ x"$1" = "xsuspend" ] && systemctl suspend
