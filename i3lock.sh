ed92621133e208099b474f8ad7dcbab712a5a74c
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2020-03-23 21:14:31 +0100

    add i3 configs

--- i3lock.sh
+++ i3lock.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xrandr --listactivemonitors > /tmp/i3lock_info
+#xrandr --listactivemonitors > /tmp/i3lock_info
 if (xrandr --listactivemonitors | grep eDP-1); then
-	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_nb.png
+	i3lock -c 000000 -i ~/Bilder/lockscreen_nb.png
 else
 	[ $(date +%H%M) -ge 1815 ] && [ x"$1" != "xsuspend" ] && DPMS=" -d"
-	i3lock -c 000000 -i ~/Pictures/wallpaper/lockscreen_desktop.png $DPMS
+	i3lock -c 000000 -i ~/Bilder/lockscreen_desktop.png $DPMS
 fi
 
 [ x"$1" = "xsuspend" ] && systemctl suspend
